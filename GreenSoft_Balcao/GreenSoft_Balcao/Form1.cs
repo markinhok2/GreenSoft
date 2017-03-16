9fa6cc5e7e307fa34218549a3991e5b298a85d8e
Marcos A C Filho <marcos.aurelio@dataplace.com.br>
2017-03-16 01:00:39 -0300

    initial commit

---
+++ GreenSoft_Balcao/GreenSoft_Balcao/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GreenSoft_Balcao.Data;
+
+namespace GreenSoft_Balcao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (dbGreenSoftFinalEntities dx = new dbGreenSoftFinalEntities())
+            {
+                Cooperativa cooperativa = new Cooperativa();
+                cooperativa.Razao = "Cooperativa de Sucata de Garça";
+                cooperativa.Cnpj = "25626430000144";
+                cooperativa.Ativo = true;
+                dx.Cooperativa.Add(cooperativa);
+                dx.SaveChanges();
+            }
+
+        }
+    }
+}
